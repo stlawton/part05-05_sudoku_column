38dc87dbfb95f5dc441280ece5b60337bf40f497
stlawton <lawtons347@gmail.com>
2025-08-19 19:34:38 -0400

    Updated code

--- src/sudoku_column.py
+++ src/sudoku_column.py
@@ -1,9 +1,9 @@
 def column_correct(sudoku: list, column_no: int):
   column_length = len(sudoku) # column length is the same as the number of rows in sudoku
   number = 1 # Variable will be used to check for each number 1-9
-  row = 0 # Variable to move through each row
   checked_number = [] # Store numbers that have already been checked. 
   while number <= column_length:
+    row = 0 # Variable to move through each row
     while row < column_length:
       if number == sudoku[row] [column_no]: # Check to see if numbers 1-9 are in the column
         if checked_number.count(number) > 0: # Check to see if any number has been repeated
@@ -12,6 +12,8 @@ def column_correct(sudoku: list, column_no: int):
           checked_number.append(number) # If the number is in the column only once, add to checked_number list
           break
       row += 1
-      return False # If any number 1-9 is NOT found in the column return false
     number += 1
+  if len(checked_number) < 9:
+    return False
   return True # If loop completes then each number was found only once in the column, return true. 
+
